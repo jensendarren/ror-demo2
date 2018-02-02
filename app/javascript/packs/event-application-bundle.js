2a3a7c34f7ad7f5279cce69298f8e0e00f4f65e1
Darren Jensen <darren.jensen@gmail.com>
2018-02-02 09:17:01 +0700

    Intalled bootstrap via Yarn

--- app/javascript/packs/event-application-bundle.js
+++ app/javascript/packs/event-application-bundle.js
@@ -1,6 +1,4 @@
 import ReactOnRails from 'react-on-rails';
-import "../../assets/stylesheets/event-application.scss"
-// https://stackoverflow.com/questions/43824626/rails-5-1-webpack-images-in-css
 
 import EventApplication from '../bundles/EventApplication/components/EventApplication';
 import EventTable from '../bundles/EventApplication/components/EventTable';
